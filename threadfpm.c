c6586203362243fa9ab5638bfc90038311c569c6
阿宝 <talent518@yeah.net>
2020-12-03 22:36:08 +0800

    return value type.

--- threadfpm.c
+++ threadfpm.c
@@ -1661,7 +1661,7 @@ static PHP_FUNCTION(share_var_exists)
 	efree(arguments);
 }
 
-static void hash_table_to_zval(bucket_t *p, zval *a) {
+static int hash_table_to_zval(bucket_t *p, zval *a) {
 	if(p->nKeyLength == 0) {
 		switch(p->value.type) {
 			case NULL_T:
@@ -1751,6 +1751,8 @@ static void hash_table_to_zval(bucket_t *p, zval *a) {
 				break;
 		}
 	}
+	
+	return HASH_TABLE_APPLY_KEEP;
 }
 
 static PHP_FUNCTION(share_var_get)
