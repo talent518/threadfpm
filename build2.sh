4f42c6f5471b994070ad122cd3a4467ec001cb9d
abao <talent518@yeah.net>
2020-12-20 20:04:37 +0800

    termux build2.sh

---
+++ build2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+
+if [ -z "$ANDROID_ROOT" -o ! "$HOME" = "/data/data/com.termux/files/home" ]; then
+    echo "Isn't android termux"
+    exit 1
+fi
+
+php=${1:-phpts}
+opt=$(dirname $PREFIX)/opt
+
+EXTENSION_DIR=$opt/$php/lib/extensions ./configure CFLAGS=-O2 CXXFLAGS=-O2 --prefix=$opt/$php --with-config-file-path=$opt/$php/etc --with-config-file-scan-dir=$opt/$php/etc/php.d --enable-zts --enable-threadfpm --enable-threadfpm-debug --enable-embed --enable-fpm --enable-phpdbg --with-openssl --with-kerberos --with-system-ciphers --with-zlib --enable-bcmath --with-bz2=$PREFIX --enable-calendar --with-curl --enable-dba=shared --enable-exif --with-ffi --enable-ftp --enable-gd --with-external-gd --with-webp --with-jpeg --with-xpm --with-freetype --enable-gd-jis-conv --with-gmp --with-mhash --with-kerberos --enable-mbstring --disable-mbregex --with-mysqli --enable-pcntl --with-pdo-mysql --with-libedit --with-readline --enable-soap --enable-sockets --with-tidy=$PREFIX --with-expat --with-xsl --enable-zend-test=shared --with-zip --enable-mysqlnd --with-iconv=$PREFIX $ZTS && make -j4 && make install
